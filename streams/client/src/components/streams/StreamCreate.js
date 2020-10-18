2eb8ecee018935d7b898146d046688d34eb462cd
Andre MacNamara <andremacnamara12@gmail.com>
2020-10-18 16:24:05 +0100

    Up to 337. Deleting Properties with Omit

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -25,10 +27,9 @@ class StreamCreate extends React.Component {
         );
     };
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit = formValues => {
+        this.props.createStream(formValues);
     }
- 
 
     render() {
         return (
@@ -57,7 +58,9 @@ const validate = (formValues) => {
 }
 
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxTunnk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxTunnk)));
 
 ReactDOM.render(
     <Provider store={store}>
