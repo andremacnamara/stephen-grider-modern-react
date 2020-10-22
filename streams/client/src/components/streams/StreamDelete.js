af1062a6922c04dad9ac0e3736fc69d23dcd0fd5
Andre MacNamara <andremacnamara12@gmail.com>
2020-10-22 20:54:31 +0100

    Using React Portals

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history'
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-    return (
-        <div>
-            StreamDelete
-        </div>
-    );
-};
+class StreamDelete extends React.Component {
 
-export default StreamDelete;
+    componentDidMount() {
+        
+        this.props.fetchStream(this.props.match.params.id);
+        console.log(this.props);
+    }
+
+
+    renderActions() {
+        const { id } = this.props.match.params;
+
+        return (
+            <React.Fragment>
+                <button 
+                    onClick={() => this.props.deleteStream(id)} 
+                    className="ui button negative"
+                >
+                    Delete
+                </button>
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent(){
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream';
+        }
+
+        return `Are you sure you want to delete this stream with the title ${this.props.stream.title}`;
+    }
+
+    render() {
+        return (
+            <Modal 
+                title="Delete Streams"
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -12,8 +12,18 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <Link to={`/stream/edit/${stream.id}`}className="ui button primary">Edit</Link>
-                    <button className="ui button negative">Delete</button>
+                    <Link 
+                        to={`/stream/edit/${stream.id}`} 
+                        className="ui button primary"
+                    >
+                        Edit
+                    </Link>
+                    <Link 
+                        to={`/stream/delete/${stream.id}`} 
+                        className="ui button negative"
+                    >
+                        Delete
+                    </Link>
                 </div>
             );
         }
